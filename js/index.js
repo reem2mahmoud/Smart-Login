099be43c2264070b2e10e7b06938037d6525b9d7
reemmahmoud <reemmahmoud726@gmail.com>
2023-07-31 22:26:08 +0200

    check page name

--- js/index.js
+++ js/index.js
@@ -12,90 +12,96 @@ var repeated = false;
 var exist = false;
 
 // SIGN-IN
-sign_in_btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  users = JSON.parse(localStorage.getItem("users"));
-  if (users) {
-    for (var i = 0; i < users.length; i++) {
-      if (
-        users[i].email == inputs[0].value &&
-        users[i].password == inputs[1].value
-      ) {
-        exist = true;
-        localStorage.setItem("user", JSON.stringify(users[i]));
+if (window.location.pathname == "/index.html") {
+  sign_in_btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    users = JSON.parse(localStorage.getItem("users"));
+    if (users) {
+      for (var i = 0; i < users.length; i++) {
+        if (
+          users[i].email == inputs[0].value &&
+          users[i].password == inputs[1].value
+        ) {
+          exist = true;
+          localStorage.setItem("user", JSON.stringify(users[i]));
+        }
       }
-    }
-    if (exist) {
-      swal(
-        "Congratulation",
-        `${inputs[0].value} successfully loggined`,
-        "success"
-      );
+      if (exist) {
+        swal(
+          "Congratulation",
+          `${inputs[0].value} successfully loggined`,
+          "success"
+        );
 
-      window.location.replace("pages/welcome.html");
+        window.location.replace("pages/welcome.html");
+      } else {
+        swal("Email or Password not valid", "Please try again later", "error");
+      }
     } else {
-      swal("Email or Password not valid", "Please try again later", "error");
+      swal(
+        "User not stored",
+        "Please go sign up page and stored your record",
+        "error"
+      );
     }
-  } else {
-    swal(
-      "User not stored",
-      "Please go sign up page and stored your record",
-      "error"
-    );
-  }
-});
+  });
+}
 
 // SIGN-UP
-sign_up_btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  for (var i = 0; i < inputs.length; i++) {
-    if (!inputs[i].value) {
-      addError(i, "");
-    } else {
-      if (inputs[i].type == "email") {
-        checkEmailValidation(inputs[i].value)
-          ? deleteError(i)
-          : addError(i, "Please enter valid email");
-      } else if (inputs[i].type == "password") {
-        checkPassValidation(inputs[i].value)
-          ? deleteError(i)
-          : addError(
-              i,
-              "Please enter valid password with (8-30 char) 1 upper, 1 lower , 1 num , 1 special char "
-            );
-      }
-    }
-  }
-  if (valid_inputs) {
-    var user = {
-      name: inputs[0].value,
-      email: inputs[1].value,
-      password: inputs[2].value,
-    };
-
-    if (JSON.parse(localStorage.getItem("users"))) {
-      console.log("here");
-      users = JSON.parse(localStorage.getItem("users"));
-      for (var i = 0; i < users.length; i++) {
-        if (users[i].email == user.email) {
-          repeated = true;
+if (window.location.pathname == "/pages/sign_up.html") {
+  sign_up_btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    for (var i = 0; i < inputs.length; i++) {
+      if (!inputs[i].value) {
+        addError(i, "");
+      } else {
+        if (inputs[i].type == "email") {
+          checkEmailValidation(inputs[i].value)
+            ? deleteError(i)
+            : addError(i, "Please enter valid email");
+        } else if (inputs[i].type == "password") {
+          checkPassValidation(inputs[i].value)
+            ? deleteError(i)
+            : addError(
+                i,
+                "Please enter valid password with (8-30 char) 1 upper, 1 lower , 1 num , 1 special char "
+              );
         }
       }
     }
+    if (valid_inputs) {
+      var user = {
+        name: inputs[0].value,
+        email: inputs[1].value,
+        password: inputs[2].value,
+      };
 
-    !repeated
-      ? addInStorge(user)
-      : addError(1, "this email already exist , choose another one");
-  }
-});
+      if (JSON.parse(localStorage.getItem("users"))) {
+        console.log("here");
+        users = JSON.parse(localStorage.getItem("users"));
+        for (var i = 0; i < users.length; i++) {
+          if (users[i].email == user.email) {
+            repeated = true;
+          }
+        }
+      }
+
+      !repeated
+        ? addInStorge(user)
+        : addError(1, "this email already exist , choose another one");
+    }
+  });
+}
 
 //LOGOUT
-logout_btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("logout btn");
-  localStorage.removeItem("user");
-  window.location.replace("../index.html");
-});
+if (window.location.pathname == "/pages/welcome.html") {
+  logout_btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("logout btn");
+    localStorage.removeItem("user");
+    window.location.replace("../index.html");
+  });
+}
 
 //CREATE USER AND ADD IN STORAGE
 function addInStorge(user) {
@@ -141,9 +147,11 @@ function deleteError(i) {
 }
 
 //USER-NAME IN WELCOME PAGE
-if (user_name) {
-  console.log("username");
-  var user = JSON.parse(localStorage.getItem("user"));
-  console.log(user.name, "user.name");
-  user_name.innerText = user.name;
+if (window.location.pathname == "/pages/welcome.html") {
+  if (user_name) {
+    console.log("username");
+    var user = JSON.parse(localStorage.getItem("user"));
+    console.log(user.name, "user.name");
+    user_name.innerText = user.name;
+  }
 }
